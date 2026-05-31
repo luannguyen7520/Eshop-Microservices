412b67377ba1dfd7821bc2ee9be7ed5847ce28ed
Nguyen Cong Luan <luannguyen7520@gmail.com>
2026-05-31 19:13:27 +0700

    remove unnecessary validation

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -8,14 +8,6 @@ namespace Catalog.API.Products.DeleteProduct;
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool IsSuccess);
 
-public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
-{
-    public DeleteProductCommandValidator()
-    {
-        RuleFor(command => command.Id).NotEmpty().WithMessage("Product Id is required");
-    }
-}
-
 public class DeleteProductHandler(IDocumentSession session) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
 {
     public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
