f2f7029caa346557c6d5a7136717faef3fc50558
Nguyen Cong Luan <luannguyen7520@gmail.com>
2026-06-03 20:33:06 +0700

    [discount] auto migrate database

--- src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
@@ -7,4 +7,14 @@ namespace Discount.Grpc.Data;
 public class DiscountDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Coupon> Coupons { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        List<Coupon> coupons = [
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+        ];
+
+        modelBuilder.Entity<Coupon>().HasData(coupons);
+    }
 }
---
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public static class Extensions
+{
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountDbContext>();
+        dbContext.Database.MigrateAsync();
+
+        return app;
+    }
+}
