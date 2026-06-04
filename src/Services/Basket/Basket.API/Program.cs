d7ab251ff2f5efd1fc2ddb2dc420baa6bbf08a43
Nguyen Cong Luan <luannguyen7520@gmail.com>
2026-06-04 21:43:18 +0700

    [basket] fix ssl certificate

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -35,7 +35,15 @@ builder.Services.AddStackExchangeRedisCache(options =>
 #region Grpc Services
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
-   options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!); 
+   options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+}).ConfigurePrimaryHttpMessageHandler(() =>
+{
+   var handler = new HttpClientHandler
+   {
+       ServerCertificateCustomValidationCallback =
+       HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+   };
+   return handler;
 });
 #endregion
 
